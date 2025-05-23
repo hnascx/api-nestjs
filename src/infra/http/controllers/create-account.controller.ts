be2ae16a519de5e72081afeb0e167823ae496454
hnascx <henrique.nascimento12@hotmail.com>
2025-05-23 11:55:33 -0300

    feat: Create infra layer

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,5 +1,4 @@
-import { ZodValidationPipe } from '@/pipe/zod-validation-pipe'
-import { PrismaService } from '@/prisma/prisma.service'
+import { PrismaService } from '@/infra/prisma/prisma.service'
 import {
   Body,
   ConflictException,
@@ -10,6 +9,7 @@ import {
 } from '@nestjs/common'
 import { hash } from 'bcryptjs'
 import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
