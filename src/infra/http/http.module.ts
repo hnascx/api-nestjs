94836c650ce4aaeae2774bce4e7e43568fed5fa8
hnascx <henrique.nascimento12@hotmail.com>
2025-05-23 12:18:37 -0300

    feat: Create prisma repositories

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService],
+  providers: [],
 })
 export class HttpModule {}
