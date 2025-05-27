6e8d2f5a46ddf50be41e225a1b05b4ad1e6150ab
hnascx <henrique.nascimento12@hotmail.com>
2025-05-27 17:50:25 -0300

    feat: Create module to define private routes by default

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,12 +3,12 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { Module } from '@nestjs/common'
+import { CryptographyModule } from '../cryptography/cryptography.module'
 import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { CryptographyModule } from '../cryptography/cryptography.module'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
