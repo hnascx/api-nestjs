fd7ec9906b9fd0464c2025fedaba3eb0ab29b096
hnascx <henrique.nascimento12@hotmail.com>
2025-05-29 11:52:34 -0300

    refactor: Add factories in E2E tests

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -5,6 +5,9 @@ import {
   AnswerComment,
   AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment'
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswerComment(
   override: Partial<AnswerCommentProps> = {},
@@ -21,4 +24,19 @@ export function makeAnswerComment(
   )
 
   return answerComment
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerComment(data: Partial<AnswerCommentProps> = {}) {
+    const answerComment = makeAnswerComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+    })
+
+    return answerComment
+  }
 }
